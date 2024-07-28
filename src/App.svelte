625c1446e4a63e6dd4e4ad23d28be27d98a833a9
Angel Bran <angel.bran@goes.gob.sv>
2024-07-28 09:12:52 -0600

    build: :sparkles: init

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<main>
+  <div>
+    <img src="/favicon.webp" class="logo" alt="Logo de El Salvador">
+  </div>
+  <h1>AB PERSONAL PROJECTS</h1>
+</main>
+
+<style>
+  .logo {
+    height: 10em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+</style>
